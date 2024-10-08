bb0c2a0dadb05464612b7aef2ca3589293f4a8bc
Beliczky Zsolt <beliczkyzsolt@gmail.com>
2024-10-08 11:09:03 +0200

    FIX2

--- helper.js
+++ helper.js
@@ -1,20 +1,2 @@
 const locations = require('./locations.json')
 const APIAdress = 'https://archive-api.open-meteo.com/v1/archive?';
-
-/* const func = async () => {
-    const p = await fetch("https://api.open-meteo.com/v1/forecast?latitude=47.4984&longitude=19.0404&hourly=temperature_2m&start_date=2024-09-02&end_date=2024-09-02").then(data => data.json()).catch((e) => console.log(e))
-    console.log(p)
-    const p2 = new Promise();
-    p2.
-}
-func(); */
-
-/* const xhr = new XMLHttpRequest();
-//meg kell mondanom hova menjen mit csináljon
-xhr.open("GET", "link");
-//meg kell mondanom neki, ha visszajött mit csináljon
-xhr.onload = function () {
-    console.log(xhr.responseText)
-}
-//ki kell neki a "menj" parancsot
-xhr.send() */
